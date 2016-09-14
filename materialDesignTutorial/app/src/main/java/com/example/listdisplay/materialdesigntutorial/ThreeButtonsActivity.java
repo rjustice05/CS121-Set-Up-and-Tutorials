9692476d1ac7bf2dfcdd0be43d3da3d72220570c
rjustice05 <rjustice@hmc.edu>
2016-09-13 22:47:46 -0700

    Added a new tutorial

---
+++ materialDesignTutorial/app/src/main/java/com/example/listdisplay/materialdesigntutorial/ThreeButtonsActivity.java
@@ -0,0 +1,13 @@
+package com.example.listdisplay.materialdesigntutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThreeButtonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_buttons);
+    }
+}
